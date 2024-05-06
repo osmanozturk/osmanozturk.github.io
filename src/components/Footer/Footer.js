9e259fb3a476cebcaba7764e1f967b6f3dab3662
osmanozturk <osmanozturkk16@outlook.com>
2024-05-06 02:15:05 +0200

    Adjust footer info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,12 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
+    <div>Special thanks to Raj Shekhar for the initial template</div>
     <a
       href='https://github.com/osmanozturk'
       className='link footer__link'
     >
-      Special thanks to Raj Shekhar
+      Osman Öztürk
     </a>
   </footer>
 )
